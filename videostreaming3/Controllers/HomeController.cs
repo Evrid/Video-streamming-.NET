fb0f78f428b79b3104555ab6f538f6869e1c33cc
Kevin Li <evrid6@gmail.com>
2023-07-22 19:21:09 +0200

    Add project files.

---
+++ videostreaming3/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using videostreaming3.Models;
+
+namespace videostreaming3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SurgeonsS1()
+        {
+            return View();
+        }
+
+        public IActionResult SurgeonsS2()
+        {
+            return View();
+        }
+
+        public IActionResult SurgeonsS3()
+        {
+            return View();
+        }
+
+        public IActionResult Lolita1997()
+        {
+            return View();
+        }
+        public IActionResult Secrets()
+        {
+            return View();
+        }
+
+        public IActionResult Aladdin()
+        {
+            return View();
+        }
+
+        public IActionResult Lupin()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
